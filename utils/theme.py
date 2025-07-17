f2b854d3077b37e33a54e9ec0d37ad1f31b78861
richiegreene <richiepgreene@gmail.com>
2025-07-17 16:08:42 -0700

    is_dark_theme() always returns true and commenting out the from Foundation import NSUserDefaults line— tristan recomendation, to remove  macOS library specific Foundation

--- utils/theme.py
+++ utils/theme.py
@@ -1,10 +1,5 @@
 import sys
-from Foundation import NSUserDefaults
+# from Foundation import NSUserDefaults
 
 def is_dark_theme():
-    try:
-        defaults = NSUserDefaults.standardUserDefaults()
-        style = defaults.stringForKey_("AppleInterfaceStyle")
-        return style == "Dark"
-    except Exception as e:
-        return False
+    return True
